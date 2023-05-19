3b1c2c4cca1d6944a25faba37f71bd89adbe0784
LNAKhoi <93086028+LNAKhoi@users.noreply.github.com>
2023-05-19 13:08:39 +0700

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,8 +9,20 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+    ) { }
 
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address:'',
+    phoneNumber: ''
+  });
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    window.alert("Your order has been submitted!");
+    this.checkoutForm.reset();
+  };
 }
